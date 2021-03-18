a0276d06bbbab4097237636da5dd387076adaa01
gondhes <fajarwirazdi@gmail.com>
2021-03-19 00:37:43 +0700

    edit form style

--- src/components/NameForm.vue
+++ src/components/NameForm.vue
@@ -24,4 +24,24 @@ h3 {
 a {
   color: #42b983;
 }
+.form {
+  border-radius: 10px;
+  border: 2px solid #73AD21;
+  padding: 20px;
+  width: 200px;
+  height: 25px;
+}
+button {
+  background-color: #3498db;
+  border: none;
+  color: white;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 10px;
+}
 </style>
