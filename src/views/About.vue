ef06d1d8b7656654b443b5d2fe4fcf883f02cd1b
fahmialis <fsinergia@yahoo.com>
2021-03-19 00:58:48 +0700

    add sound

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,15 +4,15 @@
       <div class="row justify-content-center">
         <div class="col-sm-4">
           <div class="row">
-            <div class="col-4 block-play" @click="insertTicTac(0)" @click.prevent="playSound('http://soundbible.com/mp3/Elevator Ding-SoundBible.com-685385892.mp3')" ><h1>{{$store.state.position[0].el}}</h1></div>
-            <div class="col-4 block-play" @click="insertTicTac(1)" @click.prevent="playSound('http://soundbible.com/mp3/Elevator Ding-SoundBible.com-685385892.mp3')" ><h1>{{$store.state.position[1].el}}</h1></div>
-            <div class="col-4 block-play" @click="insertTicTac(2)" @click.prevent="playSound('http://soundbible.com/mp3/Elevator Ding-SoundBible.com-685385892.mp3')" ><h1>{{$store.state.position[2].el}}</h1></div>
-            <div class="col-4 block-play" @click="insertTicTac(3)" @click.prevent="playSound('http://soundbible.com/mp3/Elevator Ding-SoundBible.com-685385892.mp3')" ><h1>{{$store.state.position[3].el}}</h1></div>
-            <div class="col-4 block-play" @click="insertTicTac(4)" @click.prevent="playSound('http://soundbible.com/mp3/Elevator Ding-SoundBible.com-685385892.mp3')" ><h1>{{$store.state.position[4].el}}</h1></div>
-            <div class="col-4 block-play" @click="insertTicTac(5)" @click.prevent="playSound('http://soundbible.com/mp3/Elevator Ding-SoundBible.com-685385892.mp3')" ><h1>{{$store.state.position[5].el}}</h1></div>
-            <div class="col-4 block-play" @click="insertTicTac(6)" @click.prevent="playSound('http://soundbible.com/mp3/Elevator Ding-SoundBible.com-685385892.mp3')" ><h1>{{$store.state.position[6].el}}</h1></div>
-            <div class="col-4 block-play" @click="insertTicTac(7)" @click.prevent="playSound('http://soundbible.com/mp3/Elevator Ding-SoundBible.com-685385892.mp3')" ><h1>{{$store.state.position[7].el}}</h1></div>
-            <div class="col-4 block-play" @click="insertTicTac(8)" @click.prevent="playSound('http://soundbible.com/mp3/Elevator Ding-SoundBible.com-685385892.mp3')" ><h1>{{$store.state.position[8].el}}</h1></div>
+            <div class="col-4 block-play" @click="insertTicTac(0)" @click.prevent="playSound(soundO)" ><h1>{{$store.state.position[0].el}}</h1></div>
+            <div class="col-4 block-play" @click="insertTicTac(1)" @click.prevent="playSound(soundX)" ><h1>{{$store.state.position[1].el}}</h1></div>
+            <div class="col-4 block-play" @click="insertTicTac(2)" @click.prevent="playSound(soundO)" ><h1>{{$store.state.position[2].el}}</h1></div>
+            <div class="col-4 block-play" @click="insertTicTac(3)" @click.prevent="playSound(soundX)" ><h1>{{$store.state.position[3].el}}</h1></div>
+            <div class="col-4 block-play" @click="insertTicTac(4)" @click.prevent="playSound(soundO)" ><h1>{{$store.state.position[4].el}}</h1></div>
+            <div class="col-4 block-play" @click="insertTicTac(5)" @click.prevent="playSound(soundX)" ><h1>{{$store.state.position[5].el}}</h1></div>
+            <div class="col-4 block-play" @click="insertTicTac(6)" @click.prevent="playSound(soundO)" ><h1>{{$store.state.position[6].el}}</h1></div>
+            <div class="col-4 block-play" @click="insertTicTac(7)" @click.prevent="playSound(soundX)" ><h1>{{$store.state.position[7].el}}</h1></div>
+            <div class="col-4 block-play" @click="insertTicTac(8)" @click.prevent="playSound(soundO)" ><h1>{{$store.state.position[8].el}}</h1></div>
           </div>
         </div>
       </div>
@@ -25,7 +25,8 @@ export default {
   data () {
     return {
       soundX: 'http://soundbible.com/mp3/Air Plane Ding-SoundBible.com-496729130.mp3',
-      soundO: 'http://soundbible.com/mp3/Elevator Ding-SoundBible.com-685385892.mp3'
+      soundO: 'http://soundbible.com/mp3/Elevator Ding-SoundBible.com-685385892.mp3',
+      song: ''
     }
   },
   methods: {
@@ -37,10 +38,13 @@ export default {
     },
     playSound (sound) {
       if (sound) {
-        var audio = new Audio(sound)
+        const audio = new Audio(sound)
         audio.play()
       }
     }
+  },
+  created () {
+    this.playSound(this.song)
   }
 }
 </script>
